603b9f3aae793ca15b67a060380699a1f6cfe3e2
J-Dizzle661 <jason.alcaraz.411@my.csun.edu>
2025-03-21 15:38:46 -0700

    Create Operations.py

---
+++ Lexer/Operations.py
@@ -0,0 +1,20 @@
+class Op:
+    def __init__(self):
+        pass
+
+class DivOp(Op):
+    def __init__(self):
+        pass
+
+class MinusOp(Op):
+    def __init__(self):
+        pass
+
+class MultOp(Op):
+    def __init__(self):
+        pass
+
+class PlusOp(Op):
+    def __init__(self):
+        pass
+
