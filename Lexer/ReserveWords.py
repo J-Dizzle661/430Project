a95de3d2a4b51b0a6e17394f6a645740929801b6
J-Dizzle661 <jason.alcaraz.411@my.csun.edu>
2025-03-26 20:24:36 -0700

    Update ReserveWords.py
    fixing issues with super class and fixes print token

--- Lexer/ReserveWords.py
+++ Lexer/ReserveWords.py
@@ -2,63 +2,63 @@ from Tokenizer import Token
 
 class ReserveWord(Token):
     def __init__(self, value):
-        super.__init__(value)
+        super().__init__(value)
 
 class this_token(ReserveWord):
     def __init__(self):
-        super.__init__('this')
+        super().__init__('this')
 
 class true_token(ReserveWord):
     def __init__(self):
-        super.__init__('true')
+        super().__init__('true')
 
 class false_token(ReserveWord):
     def __init__(self):
-        super.__init__('false')
+        super().__init__('false')
 
 class print_token(ReserveWord):
     def __init__(self):
-        super.__init__('println')
+        super().__init__('println')
 
 class new_token(ReserveWord):
     def __init__(self):
-        super.__init__('new')
+        super().__init__('new')
 
 class while_token(ReserveWord):
     def __init__(self):
-        super.__init__('while')
+        super().__init__('while')
 
 class break_token(ReserveWord):
     def __init__(self):
-        super.__init__('break')
+        super().__init__('break')
 
 class return_token(ReserveWord):
     def __init__(self):
-        super.__init__('return')
+        super().__init__('return')
 
 class if_token(ReserveWord):
     def __init__(self):
-        super.__init__('if')
+        super().__init__('if')
 
 class else_token(ReserveWord):
     def __init__(self):
-        super.__init__('else')
+        super().__init__('else')
 
 class method_token(ReserveWord):
     def __init__(self):
-        super.__init__('method')
+        super().__init__('method')
 
 class init_token(ReserveWord):
     def __init__(self):
-        super.__init__('init')
+        super().__init__('init')
 
 class super_token(ReserveWord):
     def __init__(self):
-        super.__init__('super')
+        super().__init__('super')
 
 class class_token(ReserveWord):
     def __init__(self):
-        super.__init__('class')
+        super().__init__('class')
 
 class Int_token(ReserveWord):
     def init(self):
