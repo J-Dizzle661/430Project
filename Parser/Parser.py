70208262c7dcffa64b94b71f4afbdffda0242311
Kelron1337 <dawsonj.ford@gmail.com>
2025-04-07 19:50:00 -0700

    Started the Parser
    started the parser

---
+++ Parser/Parser.py
@@ -0,0 +1,15 @@
+from Lexer.Tokenizer import Tokenizer
+from Lexer.Token import Token
+
+class Parser():
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.position = 0
+    
+    def read_token(self):
+        if (self.position < 0 or self.position >= len(self.tokens)):
+            raise Exception(f"Ran out of tokens")
+        else:
+          return self.tokens[self.position]
+        
+    def 
