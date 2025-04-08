c33468e1fde243133addb1ef49bf9e79c3c96a32
J-Dizzle661 <jason.alcaraz.411@my.csun.edu>
2025-04-07 20:16:17 -0700

    Update AST.py

---
+++ Parser/AST.py
@@ -0,0 +1,20 @@
+from Token import Token 
+
+class AST:
+    pass
+
+class Node:
+    def __init__(self, token):
+        self.token = token
+        self.parent = None
+        self.children = []
+
+    def __str__(self):
+        return f"{self.__class__.__name__}({self.value})"
+
+    def add_child(self, child):
+        if instanceof(child, Token):
+            self.children.append(child)
+            child.parent = self
+        else:
+            raise Exception('Tried adding ', child.__str__(), ' to ' ,self.__str__(), ' but not vaild AST Node!')
