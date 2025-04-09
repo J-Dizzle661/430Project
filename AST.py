3a2fe6fa016723a5a996224ae0afe0d544daa899
J-Dizzle661 <jason.alcaraz.411@my.csun.edu>
2025-04-08 20:08:28 -0700

    Adding most of the tree functionality

--- AST.py
+++ AST.py
@@ -12,11 +12,30 @@ class Node:
         self.children = []
 
     def __str__(self):
-        return f"{self.__class__.__name__}({self.value})"
+        return f"{self.token.__class__.__name__}({self.token.value})"
 
     def add_child(self, child):
-        if isinstance(child, Token):
+        if isinstance(child.token, Token):
             self.children.append(child)
             child.parent = self
         else:
-            raise Exception('Tried adding ', child.__str__(), ' to ' ,self.__str__(), ' but not a vaild AST Node!')
+            raise Exception('Tried adding ' + child.__str__() + ' to '+ self.__str__() +  ' but not a vaild AST Node!')
+        
+    def print_tree(self):
+        for i in range(self.get_level()):
+            print('     ', end='')
+        if not self.parent:
+            print(self.__str__())
+        else:
+            print('|__', self.__str__())
+        if self.children:
+            for child in self.children:
+                child.print_tree()
+
+    def get_level(self):
+        if self.parent:
+            return 1 + self.parent.get_level()
+        else:
+            return 0
+
+    
